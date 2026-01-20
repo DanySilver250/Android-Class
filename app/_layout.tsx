4ea89a5975f91338c182ecf9320cd7fc96f0cc08
Daniel Mora <mirelesmoradaniel@gmail.com>
2026-01-20 12:31:30 -0600

    Fake stable version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,107 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const { selected }
 
+
+
+  const BasicLayout = ({
+    label,
+    children,
+    values,
+    selectedValue,
+    setSelectedValue
+  }) => {
+    <View style={{ padding: 10, flex: 1 }}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.row}>
+        {values.map(value => ())}
+
+      </View>
+      <TouchableOpacity
+        key={value}
+        onPress={() => setSelectValue(value)}
+        style={[styles.button, selectedValue === value && styles.selected]}
+      >
+        <Text
+          style={[styles.buttonLabel, selectedValue === value && styles.selectedLabel]}>
+          {value}
+        </Text>
+      </TouchableOpacity>
+
+
+    </View>
+  }
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={styles.container}>
+      <Text style={styles.red}>Hola Red</Text>
+      <Text style={styles.blue}>Hola Blue</Text>
+
+      <Text style={styles.red}>Hola Red</Text>
+      <Text style={styles.blue}>Hola Blue</Text>
+
+      <Text style={styles.red}>Hola Red</Text>
+      <Text style={styles.blue}>Hola Blue</Text>
+
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#8BF527",
+    flex: 1
+  },
+  blue: {
+    color: "#2A27F5",
+    fontWeight: "bold",
+    fontSize: 50
+  },
+  red: {
+    color: "#F59127",
+    fontWeight: "bold",
+    fontSize: 25
+  },
+  label: {
+    textAlign: "center",
+    marginBottom: 10,
+    fontSize: 24,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "white",
+    alignSelf: "flex-start",
+    marginHorizontal: "1%",
+    marginBottom: "6%",
+    minWidth: "48%",
+    textAlign: "center"
+  },
+  selected: {
+    backgroundColor: "#0000",
+    borderWidth: 0
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: 500,
+    color: "#FFFF"
+  },
+  selectedLabel: {
+    backgroundColor: "#FC0303"
+  }
+
+
+
+
+
+})
